1edf7ff032b87a64d374d2e766a0761861a3cbf0
Wharkly-47 <130136825+Wharkly47@users.noreply.github.com>
2023-11-13 00:48:12 +0300

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -36,3 +36,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 // Fermer la connexion à la base de données
 $conn = null;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+
+    <h2>Inscription</h2>
+
+    <!-- Formulaire d'inscription -->
+    <form action="traitement_inscription.php" method="post">
+        <!-- Ajoutez ici les champs nécessaires pour l'inscription -->
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" name="nom" required>
+
+        <label for="prenom">Prénom :</label>
+        <input type="text" id="prenom" name="prenom" required>
+
+        <label for="email">Email :</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="mot_de_passe">Mot de passe :</label>
+        <input type="password" id="mot_de_passe" name="mot_de_passe" required>
+
+        <!-- Ajoutez d'autres champs ici si nécessaire -->
+
+        <!-- Bouton de soumission du formulaire -->
+        <button type="submit">S'inscrire</button>
+    </form>
+
+</body>
+</html>
