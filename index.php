bd50cac33b7667dbec725f7821360a6df073941f
Goofly <130136825+nearoofly@users.noreply.github.com>
2023-12-21 19:37:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
+<!-- Fichier: index.php -->
 <?php
 session_start();
 
 if (!isset($_SESSION['username'])) {
-    header("Location: connexion.html");
+    header("Location: inscription.html");
     exit();
 }
 ?>
 
-<!-- Contenu de la page principale -->
+<!-- Le reste du contenu de votre page index.php -->
+
+<!-- Lien vers la page externe -->
 <a href="https://nearoofly.github.io/Hdessawvideos" class="website-button" target="_blank">Accéder à la page externe</a>
