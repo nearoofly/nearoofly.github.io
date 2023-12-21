5788f69b135a524db057b7c028a401ec353d7ecf
Goofly <130136825+nearoofly@users.noreply.github.com>
2023-12-21 19:35:50 +0300

    Update inscript.php

--- inscript.php
+++ inscript.php
@@ -1,40 +1,15 @@
+<!-- Fichier: inscription.php -->
 <?php
-// Connexion à votre base de données (vous devrez configurer la connexion à votre base de données)
-$host = "#";
-$dbname = "nom_de_votre_base_de_donnees";
-$username = "votre_nom_d_utilisateur";
-$password = "votre_mot_de_passe";
+session_start();
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo "Erreur de connexion à la base de données : " . $e->getMessage();
-}
-
-// Récupérez les données du formulaire
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
+    $username = $_POST['username'];
     $email = $_POST['email'];
-    $mot_de_passe = password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT); // Hachage du mot de passe
-
-    // Insertion des données dans la base de données
-    $sql = "INSERT INTO utilisateurs (nom, prenom, email, mot_de_passe) VALUES (:nom, :prenom, :email, :mot_de_passe)";
-    $query = $conn->prepare($sql);
-    $query->bindParam(':nom', $nom);
-    $query->bindParam(':prenom', $prenom);
-    $query->bindParam(':email', $email);
-    $query->bindParam(':mot_de_passe', $mot_de_passe);
-
-    // Exécution de la requête
-    if ($query->execute()) {
-        echo "Inscription réussie !";
-    } else {
-        echo "Erreur lors de l'inscription.";
-    }
+    $password = $_POST['password'];
+    
+    // Insérez ici le code pour enregistrer les utilisateurs dans votre base de données
+    
+    header("Location: index.php");
+    exit();
 }
-
-// Fermer la connexion à la base de données
-$conn = null;
 ?>
